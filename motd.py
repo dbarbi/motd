ae45f474de45f2610007558c36fe2b448929a54b
Dirk Barbi <dirk.barbi@awi.de>
2020-10-15 13:21:48 +0200

    switching to yaml

--- motd.py
+++ motd.py
@@ -1,48 +1,111 @@
 import urllib.request
+import sys
+import yaml
 
-mypackage = 'motd'
-myversion = '1.0'
+class motd_handler:
+    def __init__(self):
+        url = 'https://raw.githubusercontent.com/dbarbi/motd/master/motd.yaml'
+        try:
+            self.motdfile = urllib.request.urlopen(url)
+        except:
+            print(f"Connection to file {url} containing update messages could not be established")
+            print("Please check by hand...")
+            self.database_connected = False
+            self.message_dict = {}
+            return 
+        self.database_connected = True
+        self.message_dict = yaml.load(self.motdfile, Loader=yaml.FullLoader)
 
-url = 'https://github.com/dbarbi/motd/motd.db'
-motdfile = urllib.request.urlopen
-for line in file:
-    decoded_line = line.decode("utf-8")
-    package, valid_versions, message = decoded_line.split()
+    def toint(self, versionstring):
+        versionparts=[]
+        versionparts = versionstring.split(".")
+        if len(versionparts) == 1:
+            versionparts.append("0")
+        if len(versionparts) == 2:
+            versionparts.append("0")
+        number = int(versionparts[2]) + int(versionparts[1])*1000 + int(versionparts[0])*1000000
+        return number
 
-    if package == mypackage and check_valid_version(myversion, valid_versions):
-        print (message)
+    def check_valid_version(self, my, versionrange):
+        if versionrange.startswith("<="):
+            lowerbound = -1
+            upperbound = self.toint(versionrange.replace("<=", "").strip())
+        if versionrange.startswith("<"):
+            lowerbound = -1
+            upperbound = self.toint(versionrange.replace("<", "").strip()) - 1
+        elif versionrange.startswith(">="):
+            lowerbound = self.toint(versionrange.replace(">=", "").strip()) + 1
+            upperbound = 1000000000
+        elif versionrange.startswith(">"):
+            lowerbound = self.toint(versionrange.replace(">", "").strip())
+            upperbound = 1000000000
+        else:
+            upperbound = lowerbound = self.toint(versionrange.replace(">", "").replace("<", "").replace("=", "").strip())
+
+        testnumber = self.toint(my)
+        if testnumber > lowerbound and testnumber < upperbound:
+            return True
+        else:
+            return False
+
+    def action_handler(self, action):
+        action = action.upper()
+        if action.startswith("DELAY"):
+            waittime = int(action.replace("DELAY", "").replace("(", "").replace(")", "").strip())
+            sleep(waittime)
+        if action.startswith("ERROR"):
+            print("Can't work under these circumstances.")
+            sys.exit(-1)
+
+    def motd_handler(self, mypackage, myversion):
+        if not self.database_connected:
+            return
+        for message in self.message_dict:
+            if self.message_dict["package"] == mypackage and \
+               self.check_valid_version(myversion, self.message_dict["versions"]):
+                   print (self.message_dict[message])
+                   self.action_handler(self.message_dict["action"])
+        return
 
 
-def check_valid_version(my, versionrange):
-    if versionrange.beginswith("<="):
-        lowerbound = -1
-        upperbound = toint(versionrange.replace("<=", "").strip())
-    if versionrange.beginswith("<"):
-        lowerbound = -1
-        upperbound = toint(versionrange.replace("<", "").strip()) - 1
-    elif versionrange.beginswith(">="):
-        lowerbound = toint(versionrange.replace(">=", "").strip()) + 1
-        upperbound = 1000000000
-    elif versionrange.beginswith(">"):
-        lowerbound = toint(versionrange.replace(">", "").strip())
-        upperbound = 1000000000
-    else:
-        upperbound = lowerbound = toint(versionrange.replace(">", "").replace("<", "").replace("=", "").strip())
-
-    testnumber = toint(my)
-    if testnumber > lowerbound and testnumber < upperbound:
-        return True
-    else:
-        return False
+def check_all_esm_packages():
+    motd = motd_handler()
+    installed_packages_and_versions = get_version_numbers()
+    for (package, version) in installed_packages_and_versions:
+        motd.motd_handler(package, version)
+    return
 
 
-def toint(versionstring):
-    versionparts=[]
-    versionparts = versionstring.split(".")
-    if len(versionparts) == 1:
-        versionparts.append("0")
-    if len(versionparts) == 2:
-        versionparts.append("0")
-    number = int(versionparts[2]) + int(versionparts[1])*1000 + int(versionparts[0])*1000000
-    return number
+def get_version_numbers():
+    import importlib
+    import pkg_resources
+    from esm_version_checker import esm_tools_modules
+    installed_packages_and_versions = []
+    for tool in esm_tools_modules:
+        version = "unknown"
+        try:
+            tool_mod = importlib.import_module(tool)
+            import_successful = True
+        except ImportError:
+            import_successful = False
+        if import_successful:
+            try:
+                version = tool_mod.__version__
+            except AttributeError:
+                try:
+                    version = pkg_resources.get_distribution(tool).version
+                except:
+                    raise
+        if not version == "unknown":
+            installed_packages_and_versions.append((tool, version))
+    return installed_packages_and_versions
+
+
+if __name__ == '__main__':
+    mypackage = 'motd'
+    myversion = '1.0'
+
+    motd = motd_handler()
+    motd.motd_handler(mypackage, myversion)
+    sys.exit(0)
 
